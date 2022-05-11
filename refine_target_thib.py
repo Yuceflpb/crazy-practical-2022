1ac066180c7c4e85881450810b69cbcfa08e3d57
TicaGit <thibaud.trinca@hotmail.fr>
2022-05-11 15:54:51 +0200

    clarity

--- refine_target_thib.py
+++ refine_target_thib.py
@@ -17,55 +17,60 @@ class State_refine_target(Enum):
     step_off_side = 2
     step_back_on_side = 3
 
+run_once_refine_target = True
+
+DISTANCE_STANDART_STEP = 0.01 #m
+
+Z_DETEC_TRESHOLD = 0.07 #m
+SLOWER_SPEED = 0.2
+FASTER_SPEED = 0.5
+OVERSHOT_DIST_FAST_DOWN = 0.1 #to measure
+OVERSHOT_DIST_SLOW_UP = 0.05  #to measure
+FASTER_SPEED = 0.5
+BOX_SIZE = 0.3
+
 
 with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
     with PositionHlCommander(scf) as pc:
-    #with PositionHlCommander(scf, x= x_init, y=y_init, z = z_box_init ,controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
         with Multiranger(scf) as multiranger:
             
+            #get from previous state
             incoming_target = "forward"
-
-            state_refine_target = State_refine_target.step_off
-            run_once_refine_target = True
-
-            DISTANCE_STANDART_STEP = 0.01 #m
-
-            Z_DETEC_TRESHOLD = 0.05 #m
-            SLOWER_SPEED = 0.2
-            OVERSHOT_DIST_FAST_DOWN = 0.1 #to measure
-            OVERSHOT_DIST_SLOW_UP = 0.05  #to measure
-            FASTER_SPEED = 0.5
-            BOX_SIZE = 0.3
 
             while 1:
                 
+                #state == refine
                 if run_once_refine_target:
                     #inits
-                    prev_down_dist = multiranger._down_distance
+                    state_refine_target = State_refine_target.step_off
+                    prev_down_dist = multiranger._down_distance #mesure distance ON the box
                     x_detec = pc._x
-                    y_detec = pc._y
+                    #y_detec = pc._y
                     y_step_off_side = None
 
                     #end inits
                     run_once_refine_target = False
 
                 if incoming_target == "forward":
-                    #comming with large speed
+                    #comming with large speed slow down
+                    pc._default_velocity = SLOWER_SPEED
+
                     #we just found target
                     if state_refine_target == State_refine_target.step_off:
                         pc.forward(DISTANCE_STANDART_STEP)
 
                         #go forward a bit until we step off target
                         if abs(multiranger._down_distance - prev_down_dist) >= Z_DETEC_TRESHOLD:
+                            
+                            prev_down_dist = multiranger._down_distance #mesure distance OFF the box
+
                             state_refine_target = State_refine_target.step_back_on
                         
-                        #maybe ajouter un compteur
-                        prev_down_dist = multiranger._down_distance
+                        #maybe ajouter un compteur, pas besoin ?? c'est la bonne dist ? init ligne 46
+                        #prev_down_dist = multiranger._down_distance
 
                     elif state_refine_target == State_refine_target.step_back_on:
                         
-                        #lower speed
-                        pc._default_velocity = SLOWER_SPEED
                         #go backward till target
                         pc.back(DISTANCE_STANDART_STEP)
 
@@ -73,6 +78,9 @@ with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
                             pc.back(BOX_SIZE/2 - OVERSHOT_DIST_SLOW_UP)
                             #corect coord for step up/down -> we know we are 15 more than detect
                             pc._x = x_detec + BOX_SIZE/2
+                            
+                            prev_down_dist = multiranger._down_distance #mesure distance ON the box
+
                             state_refine_target = State_refine_target.step_off_side
 
                     elif state_refine_target == State_refine_target.step_off_side:
@@ -80,7 +88,10 @@ with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
 
                         #go sideway a bit until we step off target
                         if abs(multiranger._down_distance - prev_down_dist) >= Z_DETEC_TRESHOLD:
+                            
                             y_step_off_side = pc._y
+                            prev_down_dist = multiranger._down_distance #mesure distance OFF the box
+                            
                             state_refine_target = State_refine_target.step_back_on_side
                     
                     elif state_refine_target == State_refine_target.step_back_on_side:
@@ -89,9 +100,10 @@ with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
                         if abs(multiranger._down_distance - prev_down_dist) >= Z_DETEC_TRESHOLD:
                             pc.left(BOX_SIZE/2 - OVERSHOT_DIST_SLOW_UP)
                             #corect coord for step up/down -> we know we are 15 more than detect
-                            pc._y = y_detec - y_step_off_side + BOX_SIZE/2
+                            pc._y = y_step_off_side + BOX_SIZE/2
                             
                             print("land_here")
+                            pc._default_velocity = FASTER_SPEED
                             #state = next
 
                 pass
