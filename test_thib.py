4d86930e7ac37a563fb6169030252952339e46b3
TicaGit <thibaud.trinca@hotmail.fr>
2022-05-19 16:52:21 +0200

    measure

--- test_thib.py
+++ test_thib.py
@@ -11,30 +11,35 @@ uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E770')
 
 cflib.crtp.init_drivers()
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
     with PositionHlCommander(scf) as pc:
     #with PositionHlCommander(scf, controller=PositionHlCommander.CONTROLLER_MELLINGER) as pc:
         #pc.forward(1)
         with Multiranger(scf) as multiranger:
             time.sleep(2)
-            pc.forward(0.1)
-            for i in range(1000):
-                #print(pc.get_position())
+            for i in range(100):
                 i+=1
-                if isinstance(multiranger._front_distance, float):
+                pc.forward(0.01)
+                meas = []
+                if isinstance(multiranger._down_distance, float):
+                    meas.append(multiranger._down_distance)
+
                     
-                    #print(multiranger._front_distance)
-                    if multiranger._front_distance < 0.8:   
-                        print('OBSTACLE!!!')       
-                        pc.right(0.05)
-                    else:
-                        pc.forward(0.05)
-                else:
-                    pc.forward(0.05)
             
                 if isinstance(multiranger._up_distance, float):
                     if multiranger._up_distance < 0.2:          
                         break
+            pc.land()
+
+            
+            axis = np.arange(len(meas))
+            ax, fig = plt.subplot(1)
+            ax = plt.plot(axis, meas)
+            plt.show()
+
 
         '''
             while 1:
