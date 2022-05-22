8c4909b4d853594b9fa7c2a46a7e07f0b0a3fcd8
arthur-andre <82572124+arthur-andre@users.noreply.github.com>
2022-05-22 11:11:36 +0200

    update arthur

--- test_avec_classes/classes/class_GoToBaseLoc.py
+++ test_avec_classes/classes/class_GoToBaseLoc.py
@@ -1,6 +1,7 @@
 
 from classes.class_State import State
 from classes.my_enum import Direction
+from classes.classes_obstacle_avoidance
 
 from enum import Enum
 
@@ -13,54 +14,55 @@ class State_gtbl(Enum):
     at_pos_no_step_found = 3
 
 class GoToBaseLoc(State):
-    def __init__(self, scf, pc, multiranger, x_init, y_init):
+    def __init__(self, scf, pc, multiranger, y_init):
         super().__init__(scf, pc, multiranger)
 
-        self.x_init = x_init
         self.y_init = y_init
 
         self.state_gtbl = State_gtbl.horiz
 
         self.z_meas_ctr = mn.MAX_CTR_Z_MEAS - 1
         self.prev_down_dist = None
+        self.cntr_vect = [0,0,0,0]
 
 
     def step(self):
 
         if self.state_gtbl == State_gtbl.horiz:
             self.go_to_y_coord()
+            return False
         elif self.state_gtbl == State_gtbl.vert:
-            self.measure_down_every_nb_step()
             self.go_to_x_coord()
+            return False
         elif self.state_gtbl == State_gtbl.step_found:
             return Direction.back #we're always comming from the back
         elif self.state_gtbl == State_gtbl.at_pos_no_step_found:
-            return -1
-
-        
-        
-        pass
-
-    #helper
+            return True
 
     def go_to_y_coord(self):
-        if self.pc._y > (self.y_init + mn.EPSILON_PREC):
-            #if no obs
-            self.pc.right(mn.DISTANCE_STANDART_STEP)
-        elif self.pc._y < (self.y_init - mn.EPSILON_PREC):
-            #if no obs
-            self.pc.left(mn.DISTANCE_STANDART_STEP)
+        if self.pc._y > ((self.y_init+mn.THRESHOLD_BASE_SEARCH_MAP) + mn.EPSILON_PREC):
+             if ((isinstance(self.multiranger._right_distance, float) and self.multiranger._right_distance < mn.THRESHOLD_SENSOR) 
+             or cntr_vect[3] == True):
+                cntr_vect = obstacle_step.avoid_right_side(Direction.back, cntr_vect, U_trajectory = False)
+            else : 
+                self.pc.right(mn.DISTANCE_STANDART_STEP)
+        elif self.pc._y < ((self.y_init+mn.THRESHOLD_BASE_SEARCH_MAP) - mn.EPSILON_PREC):
+            if ((isinstance(self.multiranger._left_distance, float) and self.multiranger._left_distance < mn.THRESHOLD_SENSOR) 
+             or cntr_vect[3] == True):
+                cntr_vect = obstacle_step.avoid_left_side(Direction.back, cntr_vect, U_trajectory = False)
+            else : 
+                self.pc.left(mn.DISTANCE_STANDART_STEP)
+            
         else:
             self.state_gtbl = State_gtbl.vert
 
     def go_to_x_coord(self):
-        if self.pc._x > (self.x_init + mn.EPSILON_PREC):
-            #if no obs
-
-            self.pc.back(mn.DISTANCE_STANDART_STEP)
-
-            if self.step_detection():
-                self.state_gtbl = State_gtbl.step_found
+        if self.pc._x > mn.BASE_ZONE_HEIGHT:
+            if ((isinstance(self.multiranger._back_distance, float) and self.multiranger._back_distance < mn.THRESHOLD_SENSOR) 
+             or cntr_vect[3] == True):
+                cntr_vect = obstacle_step.avoid_backward(Direction.right, cntr_vect, U_trajectory = True)
+            else : 
+                self.pc.back(mn.DISTANCE_STANDART_STEP)
         else: #we are at the base coord, without detecting it
             self.state_gtbl = State_gtbl.at_pos_no_step_found
 
